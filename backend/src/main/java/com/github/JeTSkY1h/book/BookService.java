b64852b6f36ab1a5b3446f622015d803194e697b
JeTSkY1h <jetsky1h@googlemail.com>
2022-07-11 18:25:47 +0200

    fixed memory Leak

--- backend/src/main/java/com/github/JeTSkY1h/book/BookService.java
+++ backend/src/main/java/com/github/JeTSkY1h/book/BookService.java
@@ -1,15 +1,14 @@
 package com.github.JeTSkY1h.book;
 
 import lombok.RequiredArgsConstructor;
-import nl.siegmann.epublib.domain.Resource;
 import nl.siegmann.epublib.epub.EpubReader;
 import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.*;
 @Service
 @RequiredArgsConstructor
@@ -26,25 +25,29 @@ public class BookService {
     }
 
     public void refresh() {
-        List<File> books = new ArrayList<>();
+        List<File> books;
         File f = new File("books/");
         books = Arrays.asList(f.listFiles());
-        for(File bookFile : books){
-            try {
-                nl.siegmann.epublib.domain.Book book = epubReader.readEpub(new FileInputStream(bookFile.getPath()));
-                Book bookRes = new Book();
-                BufferedImage buffCoverImg = ImageIO.read(book.getCoverImage().getInputStream());
-                File outputFile = new File(book.getTitle().replace(" ","")+".png");
-                ImageIO.write(buffCoverImg, "png", outputFile);
-                bookRes.setCoverPath(outputFile.getAbsolutePath());
-                bookRes.setAuthor(book.getMetadata().getAuthors().get(0).toString());
-                bookRes.setFilePath(bookFile.getPath());
-                bookRes.setGenre(book.getMetadata().getSubjects());
-                bookRes.setTitle(book.getTitle());
-                bookRepo.save(bookRes);
-            } catch(Exception e) {
-                System.err.println(e.getMessage());
-            }
-        }
+        List<Book> res = new ArrayList<>();
+       if(books != null) {
+           for (File bookFile : books) {
+               try (FileInputStream fIn = new FileInputStream(bookFile.getPath())) {
+                   nl.siegmann.epublib.domain.Book book = epubReader.readEpub(fIn);
+                   Book bookRes = new Book();
+                   BufferedImage buffCoverImg = ImageIO.read(book.getCoverImage().getInputStream());
+                   File outputFile = new File(book.getTitle().replace(" ", "") + ".png");
+                   ImageIO.write(buffCoverImg, "png", outputFile);
+                   bookRes.setCoverPath(outputFile.getAbsolutePath());
+                   bookRes.setAuthor(book.getMetadata().getAuthors().get(0).toString());
+                   bookRes.setFilePath(bookFile.getPath());
+                   bookRes.setGenre(book.getMetadata().getSubjects());
+                   bookRes.setTitle(book.getTitle());
+                   res.add(bookRes);
+               } catch (Exception e) {
+                   System.err.println(e.getMessage());
+               }
+           }
+       }
+       bookRepo.saveAll(res);
     }
 }
