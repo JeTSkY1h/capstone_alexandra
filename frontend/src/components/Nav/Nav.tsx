c6a570f1df48e46570a4a374c4ffedfee2078729
JeTSkY1h <jetsky1h@googlemail.com>
2022-08-04 15:15:24 +0200

    Added Menu to UserButton

--- frontend/src/components/Nav/Nav.tsx
+++ frontend/src/components/Nav/Nav.tsx
@@ -1,7 +1,14 @@
 import {Link, } from "react-router-dom"
 import {UserButton} from "./UserButton/UserButton"
 import Search from "../Input/Search";
-import {Box, Divider, Flex, Heading, Spacer, useColorModeValue} from "@chakra-ui/react";
+import {
+    Box,
+    Divider,
+    Flex,
+    Heading,
+    Spacer,
+    useColorModeValue
+} from "@chakra-ui/react";
 
 
 
@@ -17,12 +24,12 @@ const bg = useColorModeValue("dark.100", "dark.700")
 return (
     <Box height={"56px"} background={bg}>
 
-        <Flex alignItems={"center"}>
+        <Flex alignItems={"center"} justifyContent={"center"}>
             <Link to={"/"}>
                 <Heading as={"h1"}> Alexandra</Heading>
             </Link>
             <Spacer/>
-            <Flex gap={"4px"}>
+            <Flex gap={"4px"} alignItems={"center"} justifyContent={"center"}>
                 {!noSearch && <Search/>}
                 <UserButton/>
             </Flex>
--- frontend/src/components/Nav/UserButton/UserButton.tsx
+++ frontend/src/components/Nav/UserButton/UserButton.tsx
@@ -1,38 +1,58 @@
 import {useNavigate} from "react-router-dom";
 import {AiOutlineExperiment} from "react-icons/ai";
 import {parseJwt} from "../../../service/apiService";
-import {Box, Button, Tooltip, Text, IconButton, Flex} from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Tooltip,
+    Text,
+    IconButton,
+    Flex,
+    forwardRef,
+    BoxProps,
+    MenuButton,
+    MenuList, MenuItem, Menu
+} from "@chakra-ui/react";
 
-export const UserButton = () => {
+const ToggleButton = forwardRef<BoxProps, 'div'>((props, ref) => {
+    return (
+        <Box ref={ref} {...props} height={"65px"} padding={"1rem"}>
+            <Tooltip label={"Logout"}>
+                <Box
+                    as={"button"}>
+                    <Flex mr={"8px"}>
+                        <IconButton aria-label={""} mr="8px">
+                            <AiOutlineExperiment/>
+                        </IconButton>
+                        <Text style={{lineHeight: "40px", height: "40px"}}>{parseJwt().sub}</Text>
+                    </Flex>
+                </Box>
+            </Tooltip>
+        </Box>
+    )
+})
+
+export const UserButton =() => {
     const nav = useNavigate();
 
 
-
     return (
-        <>
-
-            {!localStorage.getItem("jwt-alexandra") ?
-                <Tooltip label={"Login"}>
-                    <Button onClick={() => nav("/login")}>Login</Button>
-                </Tooltip>
-                :
-                <Tooltip label={"Logout"}>
-                    <Box
-
-                        as={"button"}
-                        onClick={() => {
-                        localStorage.removeItem("jwt-alexandra");
-                        nav("/login");
-                    }}>
-                        <Flex mr={"8px"}>
-                            <IconButton aria-label={""} mr="8px">
-                                <AiOutlineExperiment/>
-                            </IconButton>
-                            <Text style={{lineHeight: "38px", height: "38px"}}>{parseJwt().sub}</Text>
-                        </Flex>
-                    </Box>
-                </Tooltip>
-            }
-        </>
+       <>
+           {!localStorage.getItem("jwt-alexandra") ?
+               <Button onClick={() => nav("/login")}>Login</Button>
+               :
+               <Menu>
+                   <MenuButton as={ToggleButton}/>
+                   <MenuList>
+                       <MenuItem onClick={()=>nav("/user")}>Profile</MenuItem>
+                       <MenuItem onClick={()=>{
+                           localStorage.removeItem("jwt-alexandra")
+                           nav("/login")
+                       }
+                       }>Logout</MenuItem>
+                   </MenuList>
+               </Menu>
+           }
+       </>
     )
 }
